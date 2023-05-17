b5e8253eeecb7f18cb216c89b70c13c9525decf5
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-17 19:41:15 +0530

    Adding custom errors

---
+++ exceptions/errors.go
@@ -0,0 +1,8 @@
+package exceptions
+
+const (
+	InternalServerError = "Internal Server Error"
+	BadRequestError     = "Bad Request Error"
+	MarshalError        = "Response Marshalling Error"
+	InvalidCityId       = "Invalid city_id given"
+)
