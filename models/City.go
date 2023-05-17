ffc9ef136e7537ed5e6722f4a0cbdb467c3834ce
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-17 19:40:15 +0530

    Adding relevant changes

--- models/City.go
+++ models/City.go
@@ -1,7 +1,8 @@
 package models
 
 type City struct {
-	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	State string `json:"state"`
+	Id       int        `json:"id"`
+	Name     string     `json:"name"`
+	State    string     `json:"state"`
+	Theatres []*Theatre `json:"theatres"`
 }
