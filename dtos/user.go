5ab4639d0c2a76f091b035abe9a3abb653779382
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-21 09:57:58 +0530

    Adding relevant dtos

---
+++ dtos/user.go
@@ -0,0 +1,20 @@
+package dtos
+
+type CreateUserRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Address   string `json:"address"`
+	City      string `json:"city"`
+}
+
+type UserResponse struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Address   string `json:"address"`
+	City      string `json:"city"`
+}
