56b8e5fa5f6ea6480691a0d516009358f4c40e2c
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-22 11:36:17 +0530

    Making relevant changes

---
+++ repos/interface.go
@@ -0,0 +1,77 @@
+package repos
+
+import (
+	"github.com/dunzoit/BookMyShow/dtos"
+	"github.com/dunzoit/BookMyShow/models"
+)
+
+type CityRepository interface {
+	GetCities() (*dtos.CitiesResponse, error)
+	AddCities(cities *dtos.CreateCitiesRequest) error
+	GetCity(cityId int) (*models.City, error)
+	UpdateCity(cityId int, updatedValues *models.City) error
+	DeleteCity(cityId int) error
+}
+
+type TheatreRepository interface {
+	GetTheatres(cityID int) ([]*models.Theatre, error)
+	AddTheatre(theatre *models.Theatre) error
+	GetTheatre(theatreID int) (*models.Theatre, error)
+	UpdateTheatre(theatreID int, updatedValues *dtos.TheatreRequest) error
+	DeleteTheatre(theatreID int) error
+}
+
+type ShowRepository interface {
+	GetShows() ([]*models.Show, error)
+	AddShow(show *dtos.CreateShowRequest) error
+	GetShowByID(showID int) (*models.Show, error)
+	UpdateShow(showID int, updatedValues *models.Show) error
+	DeleteShow(showID int) error
+}
+
+type MovieRepository interface {
+	GetMovies() ([]*models.Movie, error)
+	AddMovie(movie *dtos.CreateMovieRequest) error
+	GetMovieByID(movieID int) (*models.Movie, error)
+	UpdateMovie(movieID int, updatedValues *models.Movie) error
+	DeleteMovie(movieID int) error
+}
+
+type AuditoriumRepository interface {
+	GetAuditoriums() ([]*models.Auditorium, error)
+	AddAuditorium(auditorium *models.Auditorium) error
+	GetAuditorium(auditoriumID int) (*models.Auditorium, error)
+	UpdateAuditorium(auditoriumID int, updatedValues *models.Auditorium) error
+	DeleteAuditorium(auditoriumID int) error
+}
+
+type SeatRepository interface {
+	GetSeats() ([]*models.Seat, error)
+	AddSeat(seat *models.Seat) error
+	GetSeatByID(seatID int) (*models.Seat, error)
+	UpdateSeat(seatID int, updatedValues *models.Seat) error
+	DeleteSeat(seatID int) error
+}
+
+type PaymentRepository interface {
+	AddPayment(payment *models.Payment) error
+	GetPaymentByID(paymentID int) (*models.Payment, error)
+	UpdatePayment(paymentID int, updatedValues *models.Payment) error
+	DeletePayment(paymentID int) error
+}
+
+type BookingRepository interface {
+	GetBookings(userID int) ([]*models.Booking, error)
+	AddBooking(booking *models.Booking) error
+	GetBookingByID(bookingID int) (*models.Booking, error)
+	UpdateBooking(bookingID int, updatedValues *models.Booking) error
+	DeleteBooking(bookingID int) error
+}
+
+type UserRepository interface {
+	GetAllUsers() ([]*models.User, error)
+	AddUser(user *models.User) error
+	GetUserByID(userID int) (*models.User, error)
+	UpdateUser(userID int, updatedValues *models.User) error
+	DeleteUser(userID int) error
+}
