6c5c3624e775056b144c2905f0a23bcc2f7010f3
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-23 16:26:39 +0530

    Making relevant changes

---
+++ services/seat.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"github.com/dunzoit/BookMyShow/models"
+	"github.com/dunzoit/BookMyShow/repos"
+)
+
+type Seat struct {
+	repo repos.SeatRepository
+}
+
+func NewSeatService(repo repos.SeatRepository) *Seat {
+	return &Seat{
+		repo: repo,
+	}
+}
+
+func (c *Seat) GetSeats() ([]*models.Seat, error) {
+	return c.repo.GetSeats()
+}
+
+func (c *Seat) AddSeat(Seats *models.Seat) error {
+	return c.repo.AddSeat(Seats)
+}
+
+func (c *Seat) GetSeat(SeatId int) (*models.Seat, error) {
+	return c.repo.GetSeatByID(SeatId)
+}
+
+func (c *Seat) UpdateSeat(SeatId int, updatedValues *models.Seat) error {
+	return c.repo.UpdateSeat(SeatId, updatedValues)
+}
+
+func (c *Seat) DeleteSeat(SeatId int) error {
+	return c.repo.DeleteSeat(SeatId)
+}
