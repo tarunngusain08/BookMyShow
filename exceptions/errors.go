ca844ce535574e5ef48698da8204bc20f6c952df
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-23 17:42:45 +0530

    Making relevant changes

--- exceptions/errors.go
+++ exceptions/errors.go
@@ -5,4 +5,12 @@ const (
 	BadRequestError     = "Bad Request Error"
 	MarshalError        = "Response Marshalling Error"
 	InvalidCityId       = "Invalid city_id given"
+	InvalidTheatreID    = "Invalid theatre id"
+	InvalidAuditoriumID = "Invalid auditorium id"
+	InvalidMovieID      = "Invalid movie id"
+	InvalidShowID       = "Invalid movie id"
+	InvalidSeatID       = "Invalid seat id"
+	InvalidPaymentID    = "Invalid payment id"
+	InvalidBookingID    = "Invalid booking id"
+	InvalidUserID       = "Invalid user id"
 )
