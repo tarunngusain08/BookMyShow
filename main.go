88f1de78b4b003546ac1cb126243ffa283bc3645
TarunnGusain <prudent.tarun0808@gmail.com>
2023-05-25 07:14:56 +0530

    Adding routes

--- main.go
+++ main.go
@@ -11,9 +11,15 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/cities", controller.City.GetCities).Methods(http.MethodGet)
-	router.HandleFunc("/cities", controller.City.AddCities).Methods(http.MethodPost)
+	router.HandleFunc("/cities", controller.City.AddCity).Methods(http.MethodPost)
 	router.HandleFunc("/cities/{city-id}", controller.City.GetCity).Methods(http.MethodGet)
 	router.HandleFunc("/cities/{city-id}", controller.City.UpdateCity).Methods(http.MethodPut)
 	router.HandleFunc("/cities/{city-id}", controller.City.DeleteCity).Methods(http.MethodDelete)
+
+	router.HandleFunc("/theatres", controller.Theatre.GetTheatres).Methods(http.MethodGet)
+	router.HandleFunc("/theatres", controller.Theatre.AddTheatre).Methods(http.MethodPost)
+	router.HandleFunc("/theatres/{theatre-id}", controller.Theatre.GetTheatre).Methods(http.MethodGet)
+	router.HandleFunc("/theatres/{theatre-id}", controller.Theatre.UpdateTheatre).Methods(http.MethodPut)
+	router.HandleFunc("/theatres/{theatre-id}", controller.Theatre.DeleteTheatre).Methods(http.MethodDelete)
 
 }
